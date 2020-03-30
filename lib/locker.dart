ddd9a966858734054a5fb990926f2c9497ab2fc2
Kirk Linkletter <kirklinkletter@gmail.com>
2020-03-30 14:44:46 +0000

    remove locker and uuid functions

--- lib/locker.dart
+++
@@ -1,1 +0,0 @@
-export 'src/locker.dart' show Locker;
--- lib/src/uuid4.dart
+++
@@ -1,9 +0,0 @@
-import 'package:uuid/uuid.dart';
-import 'package:uuid/uuid_util.dart';
-
-String Uuid4() {
-  Uuid uuid = Uuid(options: {
-    'gnrg': UuidUtil.cryptoRNG
-  });
-  return uuid.v4().toString();
-}
